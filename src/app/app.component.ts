9b1976d0638a4edc48a694ae2a133ddfa0c1bc77
Martin Njoroge <martinn@innova.co.ke>
2024-02-10 23:56:35 +0300

    Generate component authmodal and nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AuthModalComponent } from "./user/auth-modal/auth-modal.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+      CommonModule, 
+      RouterOutlet, 
+      NavComponent, 
+      AuthModalComponent
+    ]
 })
 export class AppComponent {
   title = 'clips';
